be4971b0f41a9c8b99e2586629cc8e1c743a0eff
QueensKid <andres.ibarra0716@gmail.com>
2018-01-22 14:13:58 -0800

    added xunit and dependencies to the project.

--- MonsterWebApp/MonsterWebApp/Models/Weapons.cs
+++ MonsterWebApp/MonsterWebApp/Models/Weapons.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,17 @@ namespace MonsterWebApp.Models
 {
     public class Weapons
     {
+        public int ID { get; set; }
+
+        [Required, StringLength(25)]
+        public int WeaponID { get; set; }
+
+        [Required, StringLength(25)]
+        public string WeaponName { get; set; }
+
+        [Required, StringLength(50)]
+        public string UserName { get; set; }
+
+        
     }
 }
---
+++ MonsterWebApp/XUnitMidTermProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace XUnitMidTermProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+
+        }
+    }
+}
