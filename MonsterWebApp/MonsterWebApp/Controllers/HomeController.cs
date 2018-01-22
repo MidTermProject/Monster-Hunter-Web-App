1707e52ede7113b769b75dee5a1c59c9ff9bf3bf
QueensKid <andres.ibarra0716@gmail.com>
2018-01-22 11:17:28 -0800

    basic scaffolding completed

---
+++ MonsterWebApp/MonsterWebApp/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MonsterWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private //neednameDbContext _context;
+
+        //public HomeController(//neednameDbContext context)
+        //{
+        //    _context = context;
+        //}
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
